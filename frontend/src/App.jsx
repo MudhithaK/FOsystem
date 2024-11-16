c43eb4f96df3c475b5d633c185ee6712989bdba0
MudhithaK <mudhithakalpani08@gmail.com>
2024-11-16 17:35:46 +0000

    React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name,setName] = useState()
+  const [password,setPassword]= useState()
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    axios.post('',{name,password})
+    .then(result=> console.log(result))
+    .catch(err=> console.log(err))
+  }
 
   return (
     <>
@@ -12,9 +21,15 @@ function App() {
         
       
       <h1>CHECKING LOGIN...</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <lable>User Name </lable>
-        <input type="text" placeholder="Enter Name" name="name" ></input>
+        <input type="text" placeholder="Enter Name" name="name" autoComplete="off" onChange={(e)=>setName(e.target.value)}></input>
+
+        <br></br>
+        <lable>Password </lable>
+        <input type="text" placeholder="Enter Password" name="password" autoComlete="off" onChange={(e)=>setPassword(e.target.value)}></input>
+        <br></br>
+        <button type="submit">Login</button>
       </form>
         
        
