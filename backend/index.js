3d9d1af79b382b7fb5a192b82af86c941d6acc54
MudhithaK <mudhithakalpani08@gmail.com>
2024-11-16 17:44:03 +0000

    React

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const cors= require('cors')
 
 const app= express()
-appp.use(cors())
+app.use(cors())
 app.use(express.json)
 
-mongoose.connect('mongodb://127.0.0.1:27017/FOtest1');
+mongoose.connect("mongodb://127.0.0.1:27017/FOtest1");
 
 app.listen(3001,()=> {
     console.log("Server is running");
